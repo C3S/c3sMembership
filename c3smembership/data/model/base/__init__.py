67cfbf5de5ce5cc91549c56227d648de541e0e6a
Markus Lorenz <mrks@lrnz.name>
2018-07-21 22:38:45 +0200

    Code cleanup

--- c3smembership/data/model/base/__init__.py
+++ c3smembership/data/model/base/__init__.py
@@ -4,23 +4,22 @@ Base package for all SqlAlchemy model packages.
 
 Example::
 
-	from base import Base
+    from base import Base
 
-	class SomeModelClass(Base):
-		pass
+    class SomeModelClass(Base):
+        pass
 """
 
 from sqlalchemy.ext.declarative import declarative_base
-
-# pylint: disable=invalid-name
-Base = declarative_base()
-
-
 
 from zope.sqlalchemy import ZopeTransactionExtension
 from sqlalchemy.orm import (
     scoped_session,
     sessionmaker,
 )
+
+
+# pylint: disable=invalid-name
+Base = declarative_base()
 
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
