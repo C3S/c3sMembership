0ed8396b727bceb8c4f9c403fbfc3742a82b62bf
Markus Lorenz <mrks@lrnz.name>
2019-07-27 13:30:26 +0200

    Use form renderer for dues invoice archiving

--- c3smembership/presentation/views/dues_invoice_archiving.py
+++ c3smembership/presentation/views/dues_invoice_archiving.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Offers functionality to archive invoices.
+Offers functionality to archive invoices
 """
 
-from deform import ValidationFailure
 from pyramid.view import view_config
 
 from c3smembership.presentation.schemas.dues import create_archiving_form
+from c3smembership.presentation.multiple_form_renderer import \
+    MultipleFormRenderer
 
 
 @view_config(
@@ -14,56 +15,56 @@ from c3smembership.presentation.schemas.dues import create_archiving_form
     renderer='c3smembership:presentation/templates/pages/invoice_archiving.pt')
 def batch_archive_pdf_invoices(request):
     """
-    Generates and archives a number of invoices.
-
-    The number of invoices is expected in request.POST['count']. If not
-    specified all invoices are generated and archived.
+    Generate and archive a number of invoices
 
     Note:
         Expects the object request.registry.dues_invoice_archiving to implement
         c3smembership.business.dues_invoice_archiving.IDuesInvoiceArchiving.
     """
-    form = create_archiving_form()
-    if 'submit' in request.POST:
-        controls = request.POST.items()
-        try:
-            appstruct = form.validate(controls)
-            dues_invoice_archiving = request.registry.dues_invoice_archiving
-            count = appstruct['count']
-            message = ''
-            queue = ''
-            generated_files = []
-            if count is not None:
-                generated_files = dues_invoice_archiving.generate_missing_invoice_pdfs(
-                    count)
-                if generated_files is not None:
-                    queue = 'success'
-                    if len(generated_files) == 0:
-                        message = 'There were no invoices to be archived.'
-                    if len(generated_files) > 0:
-                        message = 'Successfully archived {0} invoices.'.format(
-                            len(generated_files))
-                        if len(generated_files) == count:
-                            message += ' There might be more invoices to be archived.'
-                        else:
-                            message += ' There are no more invoices to be ' + \
-                                'archived at the moment.'
-                else:
-                    message = 'An error occurred during archiving the invoices.'
-                    queue = 'danger'
+    form_renderer = build_form_renderer()
+    result = {'invoices': []}
+    return form_renderer.render(request, result)
 
-            request.session.flash(message, queue)
-            return {
-                'form': form.render(),
-                'invoices': generated_files
-            }
-        except ValidationFailure as validationfailure:
-            return {
-                'form': validationfailure.render(),
-                'invoices': [],
-            }
+
+def build_form_renderer():
+    """
+    Build the form renderer for the dues invoice archiving form
+    """
+    form_renderer = MultipleFormRenderer()
+    form = create_archiving_form()
+    form.formid = 'form'
+    form_renderer.add_form(form, archive_invoices)
+    return form_renderer
+
+
+def archive_invoices(request, result, appstruct):
+    """
+    Archive the invoices
+    """
+    dues_invoice_archiving = request.registry.dues_invoice_archiving
+    result['invoices'] = dues_invoice_archiving.generate_missing_invoice_pdfs(
+        appstruct['count'])
+    flash_message(request, result['invoices'], appstruct['count'])
+    return result
+
+
+def flash_message(request, generated_files, count):
+    """
+    Construct the message for invoice archiving to be displayed
+    """
+    if generated_files is not None:
+        queue = 'success'
+        if generated_files:
+            message = 'Successfully archived {0} invoices.'.format(
+                len(generated_files))
+            if len(generated_files) == count:
+                message += ' There might be more invoices to be archived.'
+            else:
+                message += ' There are no more invoices to be ' + \
+                    'archived at the moment.'
+        else:
+            message = 'There were no invoices to be archived.'
     else:
-        return {
-            'form': form.render(),
-            'invoices': [],
-        }
+        message = 'An error occurred during archiving the invoices.'
+        queue = 'danger'
+    request.session.flash(message, queue)
