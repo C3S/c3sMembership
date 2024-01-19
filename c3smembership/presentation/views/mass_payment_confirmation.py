09392a684d8901c5d1df0ac66e914fa651c250e0
Thomas Mielke <thomas.mielke@c3s.cc>
2024-01-19 14:10:24 +0100

    mass payment confirmation: made name comparision case-insensitive

--- c3smembership/presentation/views/mass_payment_confirmation.py
+++ c3smembership/presentation/views/mass_payment_confirmation.py
@@ -43,7 +43,12 @@ def mass_payment_confirmation(request):
     """
     Process payments and show results.
     """
-
+    import debugpy
+    debugpy.listen(("0.0.0.0", 5253))
+    print("Waiting for debugger attach")
+    debugpy.wait_for_client()
+    debugpy.breakpoint()
+    
     try:
         text_string = request.matchdict['text']
     except (KeyError, ValueError):
@@ -65,7 +70,7 @@ def mass_payment_confirmation(request):
                 "Giving up.",
                 'danger'
             )
-            break
+            return HTTPFound(request.route_url('error'))
         if row_number == 1:
             # Ensure this looks like a valid reduced Hibiscus csv.
             if (
@@ -81,6 +86,7 @@ def mass_payment_confirmation(request):
                     "Giving up.",
                     'danger'
                 )
+                return HTTPFound(request.route_url('error'))
         else:  # process actual values
             # CSV fields
             csv_date = row[0]
@@ -131,7 +137,9 @@ def mass_payment_confirmation(request):
             member = members.one()
 
             outcome[-1]['db_name'] = f'{member.firstname} {member.lastname}'
-            r = SequenceMatcher(None, outcome[-1]['db_name'], csv_name).ratio()
+            db_name_lower = outcome[-1]['db_name'].lower()
+            csv_name_lower = csv_name.lower()
+            r = SequenceMatcher(None, db_name_lower, csv_name_lower).ratio()
             outcome[-1]['db_name_color'] = (
                 '#%02X%02X%02X' % (256 - int(r*255), int(r*255), 0))
             date = datetime.datetime.strptime(csv_date, "%d.%m.%Y")
