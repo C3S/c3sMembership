7d14e68d10a63d50e6b8c7b4b283dc16342cddf0
Thomas Mielke <thomas.mielke@c3s.cc>
2024-02-28 18:36:22 +0100

    removed obsolete code for mass_payment_confirmation

--- c3smembership/presentation/views/toolbox.py
+++ c3smembership/presentation/views/toolbox.py
@@ -36,16 +36,6 @@ def membership_listing_date_pdf_callback(request, result, appstruct):
             date=appstruct['date']))
 
 
-def mass_payment_confirmation_callback(request, result, appstruct):
-    """
-    Forwards to the mass payment confirmation route for the given codes.
-    """
-    return HTTPFound(
-        location=request.route_url(
-            'mass_payment_confirmation',
-            text=appstruct['text']))
-
-
 def invoice_search_callback(request, result, appstruct):
     """
     Forwards to the dues tab of a member, containing this invoice.
@@ -119,8 +109,7 @@ def build_form_renderer():
         membership_listing_year_end_pdf_form,
         membership_listing_date_pdf_callback)
     form_renderer.add_form(
-        mass_payment_confirmation_form,
-        mass_payment_confirmation_callback)
+        mass_payment_confirmation_form)
     form_renderer.add_form(
         invoice_search_form,
         invoice_search_callback)
